d34568ca729c368316a26fe92ad9126f748958af
phildehovre <66724307+phildehovre@users.noreply.github.com>
2023-01-23 21:19:00 +0000

    firebase/config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -11,17 +11,26 @@ import { getStorage, ref } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = {
-    apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASURMENT_ID,
-    // databaseURL: import.meta.env.VITE_REACT_APP_DATABASEURL,
-};
+// const firebaseConfig = {
+//     apiKey: import.meta.env.VITE_REACT_APP_FIREBASE_API_KEY,
+//     authDomain: import.meta.env.VITE_REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: import.meta.env.VITE_REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: import.meta.env.VITE_REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: import.meta.env.VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: import.meta.env.VITE_REACT_APP_FIREBASE_APP_ID,
+//     measurementId: import.meta.env.VITE_REACT_APP_FIREBASE_MEASURMENT_ID,
+//     // databaseURL: import.meta.env.VITE_REACT_APP_DATABASEURL,
+// };
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAyODvzQMqjiStjTalwqg-DuLfEgLf7mIM",
+    authDomain: "fiftycal-ea02d.firebaseapp.com",
+    projectId: "fiftycal-ea02d",
+    storageBucket: "fiftycal-ea02d.appspot.com",
+    messagingSenderId: "41767595114",
+    appId: "1:41767595114:web:2ddd97fc2a35568e1b6e13",
+    measurementId: "G-LBPQHRBYNS"
+};
 
 
 // Initialize Firebase
